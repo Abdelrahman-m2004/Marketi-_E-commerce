f29ba587e53a8ef8d0d22a4c04a8cbb6d26470a0
Rewan Elsonbaty <rewanelsonbaty735@gmail.com>
2026-07-11 23:00:59 +0300

    chore: save current changes before merge

--- lib/features/home/presentation/widget/home_widget_content.dart
+++ lib/features/home/presentation/widget/home_widget_content.dart
@@ -5,16 +5,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:marketi/core/app_dependencies.dart';
 import 'package:marketi/core/constants/app_rout.dart';
-
+import 'package:marketi/features/home/presentation/cubit/brand_cubit/brand_cubit.dart';
+import 'package:marketi/features/home/presentation/cubit/category_cubit/category_cubit.dart';
+import 'package:marketi/features/home/presentation/cubit/product_cubit/product_cubit.dart';
 import 'package:marketi/features/home/presentation/widget/advertising_card.dart';
 import 'package:marketi/features/home/presentation/widget/brand_list.dart';
 import 'package:marketi/features/home/presentation/widget/build_list_of_best_for_you.dart';
 import 'package:marketi/features/home/presentation/widget/build_list_of_buy_again.dart';
-
 import 'package:marketi/features/home/presentation/widget/build_list_of_popular_product.dart';
 import 'package:marketi/features/home/presentation/widget/category_list.dart';
 import 'package:marketi/features/home/presentation/widget/custom_search_button.dart';
-
 import 'package:marketi/features/home/presentation/widget/home_appbar.dart';
 import 'package:marketi/features/home/presentation/widget/super_title_view_all.dart';
 import 'package:skeletonizer/skeletonizer.dart';
@@ -28,29 +28,37 @@ class HomeWidgetContent extends StatefulWidget {
 
 class _HomeWidgetContentState extends State<HomeWidgetContent> {
   bool timer = true;
+  late final ProductCubit _productCubit;
+  late final CategoryCubit _categoryCubit;
+  late final BrandCubit _brandCubit;
 
   @override
   void initState() {
     super.initState();
+    _productCubit = appDependencies.productCubit..getProducts();
+    _categoryCubit = appDependencies.categoryCubit..getCategories();
+    _brandCubit = appDependencies.brandCubit..getBrands();
 
     Future.delayed(const Duration(milliseconds: 2500), () {
-      setState(() {
-        timer = false;
-      });
+      if (mounted) setState(() => timer = false);
     });
+  }
+
+  @override
+  void dispose() {
+    _productCubit.close();
+    _categoryCubit.close();
+    _brandCubit.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(
-          create: (_) => appDependencies.productCubit..getProducts(),
-        ),
-        BlocProvider(
-          create: (_) => appDependencies.categoryCubit..getCategories(),
-        ),
-        BlocProvider(create: (_) => appDependencies.brandCubit..getBrands()),
+        BlocProvider.value(value: _productCubit),
+        BlocProvider.value(value: _categoryCubit),
+        BlocProvider.value(value: _brandCubit),
       ],
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -58,44 +66,40 @@ class _HomeWidgetContentState extends State<HomeWidgetContent> {
           enabled: timer,
           child: Column(
             children: [
-              SizedBox(height: 50),
-
-              HomeAppBar(
+              const SizedBox(height: 50),
+              const HomeAppBar(
                 imagePath: 'assets/image/avatare.png',
                 customerName: 'yousef',
               ),
-
               Expanded(
                 child: ListView(
                   padding: EdgeInsets.zero,
                   physics: const ClampingScrollPhysics(),
-                  shrinkWrap: true,
                   children: [
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
                     CustomSearchButton(
                       readOnly: true,
-                      ontap: () =>
-                          Navigator.pushNamed(context, AppRoute.search),
+                      ontap: () => Navigator.pushNamed(context, AppRoute.search),
                     ),
-                    SizedBox(height: 15),
-                    AdvertisingCard(),
-                    SizedBox(height: 10),
-                    SuperTitleViewAll(superTitleName: 'Popular Product'),
-                    SizedBox(height: 5),
-                    BuildListOfPopularProduct(),
-                    SizedBox(height: 10),
-                    SuperTitleViewAll(superTitleName: 'Category'),
-                    CategoryList(),
-                    SizedBox(height: 7),
-                    SuperTitleViewAll(superTitleName: 'Best For You'),
-                    BuildListOfBestForYou(),
-                    SizedBox(height: 10),
-                    SuperTitleViewAll(superTitleName: 'Brand'),
-                    BrandList(),
-                    SizedBox(height: 7),
-                    SuperTitleViewAll(superTitleName: 'Buy Again'),
-                    BuildListOfBuyAgain(),
-                    SizedBox(height: 10),
+                    const SizedBox(height: 15),
+                    const AdvertisingCard(),
+                    const SizedBox(height: 10),
+                    const SuperTitleViewAll(superTitleName: 'Popular Product'),
+                    const SizedBox(height: 5),
+                    const BuildListOfPopularProduct(),
+                    const SizedBox(height: 10),
+                    const SuperTitleViewAll(superTitleName: 'Category'),
+                    const CategoryList(),
+                    const SizedBox(height: 7),
+                    const SuperTitleViewAll(superTitleName: 'Best For You'),
+                    const BuildListOfBestForYou(),
+                    const SizedBox(height: 10),
+                    const SuperTitleViewAll(superTitleName: 'Brand'),
+                    const BrandList(),
+                    const SizedBox(height: 7),
+                    const SuperTitleViewAll(superTitleName: 'Buy Again'),
+                    const BuildListOfBuyAgain(),
+                    const SizedBox(height: 10),
                   ],
                 ),
               ),
